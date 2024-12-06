048a76e6cec89fd8a0c25da9be2ecd607a47c212
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2024-12-06 11:53:21 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
+import Pagina from "./components/Paginas";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div>
-      <header>
+  const [paginaAtual, setPaginaAtual] = useState({nome: 'Home', id: 1});
 
+  const handleChange = (event) => {
+    const nome = event.target.name;
+    const valor = event.target.value;
+    setInputs(valores => ({...valores, [nome]: valor}));
+}
+
+  return (
+    <>
+      <header>
+        
       </header>
       <main>
-
+        <Pagina
+          nome = {paginaAtual.nome}
+          id = {paginaAtual.id}
+        />
       </main>
       <footer>
         
       </footer>
-    </div>
+    </>
   );
 }
 
